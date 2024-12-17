a51e31ffb953082b57fed5ab63007cb9662c519d
crrmail <preeyamon.tester@gmail.com>
2024-12-17 14:24:07 +0700

    update/All test case

--- cypress/e2e/api-tests.cy.js
+++ cypress/e2e/api-tests.cy.js
@@ -1,34 +1,37 @@
 describe('API Test', () => {
-    it('TC001 - Create user', () => {
-        cy.request({
+    it('TC001 - Create user', async () => {
+        const response = await cy.request({
             method: 'POST',
             url: 'https://reqres.in/api/users',
             body: {
-                "name": "Davika Hone",
+                "name": "Davika Hoone",
                 "job": "Tester"
             }
-        }).then((response) => {
-            expect(response.status).to.eq(201)
-            expect(response.body).to.have.property('name','Davika Hone')
-            expect(response.body).to.have.property('job','Tester')
-            expect(response.body).to.have.property('id')
-            expect(response.body).to.have.property('createdAt')
         })
+        expect(response.status).to.eq(201)
+        expect(response.body).to.have.property('name','Davika Hoone')
+        expect(response.body).to.have.property('job','Tester')
+        expect(response.body).to.have.property('id')
+        expect(response.body).to.have.property('createdAt')
+        
     })
 
-    it('TC002 - Get Single User', () => {
-        cy.request('GET','https://reqres.in/api/users/9')
-        .then((response) => {
-            expect(response.status).to.eq(200)
-            const user = response.body.data
-            expect(user).to.have.property('id')
-            expect(user).to.have.property('email')
-            expect(user).to.have.property('first_name')
-            expect(user).to.have.property('last_name')
-            expect(user).to.have.property('avatar')
-            expect(response.body.support).to.have.property('url')
-            expect(response.body.support).to.have.property('text')
+    it('TC002 - Get Single User', async () => {
+        const response = await cy.request({
+            method: 'GET',
+            url: 'https://reqres.in/api/users/9'
+        
         })
+        expect(response.status).to.eq(200)
+        const user = response.body.data
+        expect(user).to.have.property('id')
+        expect(user).to.have.property('email')
+        expect(user).to.have.property('first_name')
+        expect(user).to.have.property('last_name')
+        expect(user).to.have.property('avatar')
+        expect(response.body.support).to.have.property('url')
+        expect(response.body.support).to.have.property('text')
+        
     })
 
     it('TC003 - Update User', async () => {
@@ -36,14 +39,14 @@ describe('API Test', () => {
             method: 'PUT',
             url: 'https://reqres.in/api/users/9',
             body: {
-                "name": "Jinda Hone",
+                "name": "Jinda Hoone",
                 "job": "Automate Tester",
             }
        })
-            expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('name','Jinda Hone')
-            expect(response.body).to.have.property('job','Automate Tester')
-            expect(response.body).to.have.property('updatedAt') 
+        expect(response.status).to.eq(200)
+        expect(response.body).to.have.property('name','Jinda Hoone')
+        expect(response.body).to.have.property('job','Automate Tester')
+        expect(response.body).to.have.property('updatedAt') 
     })
 
     it('TC004 - Delete User', async () => {
